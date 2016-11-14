a71508d49d594722dcf94529397520be64fe0992
jean <jean.ma.1986@gmail.com>
2016-11-14 16:24:53 +0800

    fix

--- src/stock.js
+++ src/stock.js
@@ -6,41 +6,28 @@ import {
 import path from "path";
 import fs from "fs";
 import xlsx from "node-xlsx";
+import df from "dateformat";
 
 export async function getStockListFromSH() {
-	let basePath = path.join(__dirname, "../build/data");
+	let basePath = path.join(__dirname, `../build/${df(new Date(), "yyyymmdd")}`);
 	let url = "https://www.szse.cn/szseWeb/ShowReport.szse?SHOWTYPE=xlsx&CATALOGID=1110&tab2PAGENUM=1&ENCODE=1&TABKEY=tab2";
 	if (!fs.existsSync(basePath)) {
 		fs.mkdirSync(basePath);
 	}
 	let fileName = path.join(basePath, "sh.xlsx");
-	console.log(`path=${fileName}`);
-	let file, text;
-	if (fs.existsSync(fileName)) {
-		let stat = fs.lstatSync(fileName);
-		let birthtimeValue = stat.birthtime.valueOf();
-		let expiretimeValue = birthtimeValue + 24 * 60 * 60 * 1000;
-		if (Date.now() > expiretimeValue) {
-			file = await downloadFileFromUrl(fileName, url);
-			text = fs.readFileSync(file);
-		}
-		else {
-			text = fs.readFileSync(fileName);
-		}
+	if (!fs.existsSync(fileName)) {
+		await downloadFileFromUrl(fileName, url);
 	}
-	else {
-		file = await downloadFileFromUrl(fileName, url);
-		text = fs.readFileSync(file);
-	}
+	let text = fs.readFileSync(fileName);
 	let buffer = xlsx.parse(text);
 	console.log(buffer[0].name);
-	buffer[0].data[0].forEach(value=> {
-		console.log(rmSpace(unicodeToUTF8(value)));
-	})
+	// buffer[0].data[0].forEach(value=> {
+	// 	console.log(rmSpace(unicodeToUTF8(value)));
+	// })
 }
 
-export function getStockHistoryFromYahoo(stockCode){
+export async function getStockHistoryFromYahoo(stockCode, startDate, endDate = new Date()) {
 	//http://blog.sina.com.cn/s/blog_7ed3ed3d010146ti.html
-	let url=`http://table.finance.yahoo.com/table.csv?s=${stockCode}`;
-	let file
+	let url = `http://table.finance.yahoo.com/table.csv?s=${stockCode}`;
+
 }
